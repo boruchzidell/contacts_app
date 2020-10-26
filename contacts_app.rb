9e59739536d340f7545b2262bd12c1922287c880
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2020-10-25 22:12:34 -0400

    working version includes frequent contacts

--- contacts_app.rb
+++ contacts_app.rb
@@ -6,7 +6,7 @@ require 'sinatra/reloader' if development?
 require 'sinatra/content_for'
 require 'tilt/erubis'
 require 'yaml'
-
+require 'pry' if development?
 configure do
   set :sessions, expire_after: 365*24*60*60
   set :session_secret, 'secret'
@@ -30,8 +30,21 @@ def save_data(data)
   end  
 end
 
+helpers do
+  # Returns array of most frequent contacts
+  def most_frequent_contacts(max)
+
+    frequent_ids = session[:tally].sort_by {|k,v| -v.to_i}.take(max).to_h.keys
+
+    frequent_ids.map do |id|
+      @list.find {|contact| contact[:guid] == id}
+    end.compact
+  end
+end
+
 before do
   @list = load_data
+  session[:tally] = {} unless session[:tally]
 end
 
 # Returns next available GUID
@@ -39,33 +52,79 @@ def generate_guid
   list = load_data
 
   max = list.max_by{|hash| hash[:guid]}
-  max ? max[:guid] + 1 : 1
+  max ? max[:guid].next : '1'
 end
 
+# Returns instance variable to populate group dropdown box in template
+def group_drop_down
+  @groups = @list.map { |hash| hash[:group] }.uniq.compact
+end
 
+# Updates the number of times a contact was accessed
+def update_tally(guid)
+  session[:tally][guid] ? session[:tally][guid] += 1 : session[:tally][guid] = 1 
+end
+
+# Passes @list to index to display all contacts
 get "/" do
+  @filter_for_group = params[:group]
+
+  # To populate filter dropdown menu
+  group_drop_down
+
+  if @filter_for_group
+    @list.select! { |contact| contact[:group] == @filter_for_group }
+  end
+
   erb :index
 end
 
-# Returns form to create new contact
+# Returns form to create or edit a contact
 get "/create" do
-
-  @groups = @list.map do |hash|
-    hash[:group]
-  end
-
+  group_drop_down
+  @contact_info = {}
   erb :create_contact
 end
 
-# Creates new contact
+# Page to display individual contact
+get "/:id" do
+  guid = params[:id]
+
+  update_tally(guid)
+
+  @contact_info = @list.find { |contact| contact[:guid] == guid }
+
+  erb :contact
+end
+
+get "/:id/edit" do
+  guid = params[:id]
+  @contact_info = @list.find { |contact| contact[:guid] == guid }
+  group_drop_down
+  erb :create_contact
+end
+
+
+# Creates or modifies a contact
 post "/contact" do
-  @list << {
-    guid: generate_guid,
+
+  guid = params[:guid]
+
+  new_contact = {
+    guid: guid || generate_guid,
     first_name: params[:first_name],
     last_name: params[:last_name],
     group: params[:group]
   }
-
+  
+  match = @list.find {|contact| contact[:guid] == guid }
+  
+  if match
+      match.merge!(new_contact)
+  else
+    @list << new_contact
+  end
+  
   save_data(@list)
 
   redirect "/"
